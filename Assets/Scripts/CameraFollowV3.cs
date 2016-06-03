3ea5b22ab8a5cf5612fade2b7c184132b95148af
leannleann <choir.forever@yahoo.com>
2016-06-03 12:14:23 -0500

    new camera
    created a new camera script. it will follow the player 1:1 except when a
    charge attack is initiated

---
+++ Assets/Scripts/CameraFollowV3.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowV3 : MonoBehaviour {
+
+	private Rigidbody2D playerRB2D;
+	public eHeroController playerCntrl;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		playerRB2D = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate()
+	{
+		transform.position = new Vector3(playerRB2D.position.x, playerRB2D.position.y, transform.position.z);
+	}
+}
