2274ec09931ea5f91dbcc37eb081f5071bae7d29
kylecruffin <kylecruffin@gmail.com>
2016-06-21 17:55:07 -0500

    Rocks and Steam Vents and iTween Oh My

---
+++ Assets/Scripts/FallingRock.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingRock : MonoBehaviour 
+{
+	public GameObject RockParticles;
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (this.gameObject.GetComponent<Renderer>().isVisible)
+		{
+			Instantiate (RockParticles, this.transform.position, this.transform.rotation);
+			GameObject cam = GameObject.FindGameObjectWithTag ("MainCamera") as GameObject;
+			iTweenEvent.GetEvent (cam, "ScreenPunch").Play ();
+		}
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/SteamVent.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteamVent : MonoBehaviour 
+{
+
+	public float sprayTimer = 0.5f;
+	public bool[] sprayTimes = new bool[8];
+
+
+	private float actualTimer;
+	private int sprayIndex;
+	private ParticleSystem steam;
+
+	void Start()
+	{
+		actualTimer = sprayTimer;
+		sprayIndex = 0;
+		steam = GetComponent<ParticleSystem> ();
+		//steam.emission.rate = 0;
+	}
+
+	void Update()
+	{
+		actualTimer -= Time.deltaTime;
+
+		if (actualTimer <= 0)
+		{
+			if (sprayTimes [sprayIndex] == true)
+			{
+				Debug.Log (actualTimer);
+				steam.Emit(10);
+			}
+			sprayIndex++;
+			if(sprayIndex > 7) 
+			{
+				sprayIndex = 0;
+			}
+			actualTimer = sprayTimer;
+		}
+	}
+
+}
