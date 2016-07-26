7afa6e1d65b3a3175a8620325e8277c1a9992067
kylecruffin <kylecruffin@gmail.com>
2016-07-25 20:00:34 -0500

    maybe this will work

---
+++ Assets/Scripts/Destructible.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destructible : MonoBehaviour {
+
+	public GameObject RockParticles;
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			if (col.gameObject.GetComponent<eHeroController>().doDash == true)
+			{
+				Instantiate (RockParticles, this.transform.position, this.transform.rotation);
+				GameObject cam = GameObject.FindGameObjectWithTag ("MainCamera") as GameObject;
+				iTweenEvent.GetEvent (cam, "ScreenPunch").Play ();
+				Destroy (gameObject);
+			}
+		}
+	}
+}
